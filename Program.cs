471540e8765ce7b0a92994354ebb409c223baf6e
waus <kamil.muranski.official@gmail.com>
2020-09-23 00:29:04 +0200

    02.Tytuł oraz 2.Tytuł

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -21,11 +17,13 @@ namespace MusicRenamer
             Regex regex2 = new Regex(@"^\d\d\.\s[\w* [(\.]+");        //02. Hey hey Oh!
             Regex regex3 = new Regex(@"^\d\d\-[\w* [(\.]+");        //02-Hey hey Oh!
             Regex regex4 = new Regex(@"^\d\d\-\s[\w* [(\.]+");        //02- Hey hey Oh!
+            Regex regex5 = new Regex(@"^\d\d\.[\w* [(\.]+");        //02.Hey hey Oh!
 
             Regex regex1a = new Regex(@"^\d\s[\w* [(\.]+");     //2 Hey hey Oh!
             Regex regex2b = new Regex(@"^\d\.\s[\w* [(\.]+");        //2. Hey hey Oh!
             Regex regex3c = new Regex(@"^\d\-[\w* [(\.]+");        //2-Hey hey Oh!
             Regex regex4d = new Regex(@"^\d\-\s[\w* [(\.]+");        //2- Hey hey Oh!
+            Regex regex5e = new Regex(@"^\d\.[\w* [(\.]+");        //2.Hey hey Oh!
 
             string path = Directory.GetCurrentDirectory();
             foreach (string filePath in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
@@ -50,6 +48,10 @@ namespace MusicRenamer
                 {
                     fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(4, fileName.Length - 4);
                 }
+                else if (regex5.IsMatch(fileName))
+                {
+                    fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(3, fileName.Length - 3);
+                }
                 else if (regex1a.IsMatch(fileName))
                 {
                     fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
@@ -65,6 +67,10 @@ namespace MusicRenamer
                 else if (regex4d.IsMatch(fileName))
                 {
                     fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(3, fileName.Length - 3);
+                }
+                else if (regex5e.IsMatch(fileName))
+                {
+                    fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
                 }
 
                 if (!String.IsNullOrEmpty(fileNameRenamed))
@@ -82,7 +88,4 @@ namespace MusicRenamer
             }
         }
     }
-
-
-
 }
